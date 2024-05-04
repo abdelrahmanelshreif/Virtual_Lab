e1a1abea3093c8465b8d8ebbf2fc81a0e31596fd
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-04 13:31:52 +0300

    Intial Auth

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,96 @@
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.deleteOne = (Model, withUser) =>
+  catchAsync(async (req, res, next) => {
+    let doc;
+    if (withUser) {
+      doc = await Model.findOneAndDelete({ user: req.user.id });
+    } else {
+      doc = await Model.findByIdAndDelete(req.params.id);
+    }
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  });
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!doc) {
+      return next(new AppError('No document Found with that ID', 404));
+    }
+    res.status(200).json({
+      status: 'sucess',
+      data: {
+        data: doc
+      }
+    });
+  });
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    let newDocData = req.body;
+    if (req.file) {
+      const buffer = req.file.buffer;
+      newDocData[req.file.fieldname] = buffer.toString('base64');
+    }
+    const newDoc = await Model.create(newDocData);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDoc
+      }
+    });
+  });
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    if (popOptions) query.populate(popOptions);
+    const features = new APIFeatures(Model.findById(req.params.id), req.query)
+      .filter()
+      .sort()
+      .fieldLimiting()
+      .paginate();
+    // const docs = await features.query.explain();
+    const doc = await features.query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc
+      }
+    });
+  });
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    // hack for get all reviews
+    let filter = {};
+    // eslint-disable-next-line no-unused-vars
+    if (req.params.userId) filter = { user: req.params.userId };
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .fieldLimiting()
+      .paginate();
+    // const docs = await features.query.explain();
+    const docs = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        docs
+      }
+    });
+  });
---
+++ server.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+process.on('uncaughtException',err=> {
+  console.log('UNCAUGHT EXCEPTION!  Shutting Down...');
+  console.log(err.name,err.message);
+  process.exit(1);
+});
+
+dotenv.config({path:'./config.env'});
+const app = require('./app');
+
+
+mongoose
+// .connect(DB
+    .connect(process.env.DATABASE_LOCAL
+  ,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false,
+  useUnifiedTopology: true
+}).then(()=>console.log('DB Connections Successful..!')).catch(err=> console.log('ERROR'));
+
+console.log(process.env.NODE_ENV);
+
+// 4) START SERVER 
+// const {port} = process.env;
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port,()=>{
+  console.log(`App Running on Port ${port}....`);
+}); 
+
+process.on('unhandledRejection',err=>{
+  console.log(err.name,err.message);
+  console.log('UNHANDELER REJECTION! SHUTTING DOWN...');
+  server.close(()=>{
+    process.exit(1);
+  })
+});
+
+
