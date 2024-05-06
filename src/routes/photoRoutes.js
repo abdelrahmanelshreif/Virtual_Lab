f54b4ce75d79f767e2f3b3afd9fc2f7dff27bf3f
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-06 06:20:48 +0300

    Test Get Photo

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -3,16 +3,16 @@ const authController = require('../controllers/authController');
 const photoController = require('../controllers/photoController');
 const router = express.Router();
 
-router.use(authController.protect);
-
-
-router.use(authController.restrictTo('teacher'));
-
-
-// Upload Photo
-router.post('/upload',photoController.uploadPhoto);
 
 // Get Photo
 router.get('/:filename',photoController.accessPhoto);
+router.use(authController.protect);
+
+
+//Only Teacher Can Upload The Photo
+router.use(authController.restrictTo('teacher'));
+// Upload Photo
+router.post('/upload',photoController.uploadPhoto);
+
 
 module.exports = router;
