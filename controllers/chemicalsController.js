cb7de4aca9f8df30108bd5e1149966d3ee702ab7
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-06 06:09:38 +0300

    Moving Files 2nd Time

--- controllers/chemicalsController.js
+++
@@ -1,7 +0,0 @@
-const factory = require('../controllers/handlerFactory');
-const Chemicals = require('../model/chemicalsModel');
-
-exports.getAllChemicals = factory.getAll(Chemicals);
-exports.createNewChemical = factory.createOne(Chemicals);
-exports.updateChemical = factory.updateOne(Chemicals);
-exports.deleteChemical = factory.deleteOne(Chemicals);
--- controllers/toolsController.js
+++
@@ -1,7 +0,0 @@
-const factory = require('../controllers/handlerFactory');
-const Tool = require('../model/toolsModel');
-
-exports.getAllTools = factory.getAll(Tool);
-exports.createNewTool = factory.createOne(Tool);
-exports.updateTool = factory.updateOne(Tool);
-exports.deleteTool = factory.deleteOne(Tool);
