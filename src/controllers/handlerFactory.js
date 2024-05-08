5681088aa338eb041bf16a9c70e5f30e2f6c0ab0
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-08 14:10:40 +0300

    upadting deleteFactory and Change Response Status Code

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -50,9 +50,10 @@ exports.deleteOne = Model =>
     if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
-    res.status(204).json({
-      status: 'success',
-      data: null
+    res.status(200).json({
+      
+      status: 'deletion success',
+      deletedObjectId: doc._id
     });
   });
 
