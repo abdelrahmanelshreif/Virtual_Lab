5d8c4faf6b6bf8921a885edceafee768dd3e4815
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-12 14:11:45 +0300

    adding controller to get user data and make a protected route for it

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,4 +7,7 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.use(authController.protect)
+router.get('/user', authController.getMe,authController.getUser);
+
 module.exports = router;
