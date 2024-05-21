681330e53e53de9f53e5c1a3f690b2eac2137f05
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-21 17:45:25 +0300

    edit ph controller\

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -7,7 +7,7 @@ const factory = require('../controllers/handlerFactory');
 
 const multerStorage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './src/uploads');
+    cb(null, './uploads');
   },
   filename: function(req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
