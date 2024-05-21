449c48403f52578a5d1af3b4be4d9bc10d66e6ad
Abdelrahman Elshreif <link93955@gmail.com>
2024-05-21 17:33:29 +0300

    Last Edition on routes

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const photoController = require('../controllers/photoController');
+const handlerFactory = require('../controllers/handlerFactory');
 const router = express.Router();
 
+
+router.get('/:filename',handlerFactory.accessPhoto);
+
 router
-  .route('/:id')
+  .route('/v2/:id')
   .get(photoController.accessPhoto)
   .delete(photoController.deletePhoto);
 router
-  .route('/')
+  .route('/v2')
   .post(photoController.createPhoto)
   .get(photoController.getAllPhotos);
 
